80d19136b3df1799be74a22acecf1c3a02b42b99
jorrel6130 <66708266+jorrel6130@users.noreply.github.com>
2022-12-11 18:48:59 -0500

    Added Restaurant Class and RestaurantList Class

--- app/src/main/java/ca/gbc/myapplication/MainActivity.java
+++ app/src/main/java/ca/gbc/myapplication/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -16,6 +17,8 @@ public class MainActivity extends AppCompatActivity {
     Button add;
     AlertDialog dialog;
     LinearLayout layout;
+
+    RestaurantList rest = new RestaurantList(1000);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,13 +46,32 @@ public class MainActivity extends AppCompatActivity {
         final EditText address = view.findViewById(R.id.adressIn);
         final EditText phone = view.findViewById(R.id.phoneIn);
         final EditText description = view.findViewById(R.id.descriptionIn);
+        final CheckBox isVegetarian = view.findViewById(R.id.tagVegetarian);
+        final CheckBox isVegan = view.findViewById(R.id.tagVegan);
+        final CheckBox isOrganic = view.findViewById(R.id.tagOrganic);
+        final CheckBox isEuropean = view.findViewById(R.id.tagEuropean);
+        final CheckBox isAsian = view.findViewById(R.id.tagAsian);
+        final CheckBox isOther = view.findViewById(R.id.tagOther);
+
+
 
         builder.setView(view);
         builder.setTitle("Enter name")
                 .setPositiveButton("OK", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        addCard(name.getText().toString());
+                        String addName = name.getText().toString();
+                        String addAddress = address.getText().toString();
+                        String addPhone = phone.getText().toString();
+                        String addDescription = description.getText().toString();
+                        Boolean addVegetarian = isVegetarian.isChecked();
+                        Boolean addVegan = isVegan.isChecked();
+                        Boolean addOrganic = isOrganic.isChecked();
+                        Boolean addEuropean = isEuropean.isChecked();
+                        Boolean addAsian = isAsian.isChecked();
+                        Boolean addOther = isOther.isChecked();
+                        rest.addRestaurant(addName, addAddress, addPhone, addDescription, addVegetarian, addVegan, addOrganic, addEuropean, addAsian, addOther);
+                        addCard(rest.findRestaurant(0).getName());
                     }
                 })
                 .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
