88c76d619389ab22efacfbce07a1edeef46abb08
ronal rodriguez arias <newronal@hotmail.com>
2022-12-11 01:06:21 -0500

    List showing test

--- app/src/main/java/ca/gbc/myapplication/MainActivity.java
+++ app/src/main/java/ca/gbc/myapplication/MainActivity.java
@@ -1,36 +1,78 @@
 package ca.gbc.myapplication;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
-
-import java.util.ArrayList;
-
 public class MainActivity extends AppCompatActivity {
-    private Button btn;
-    private TextView name,address,phone;
-    ArrayList<String> resList = new ArrayList<>();
+
+    Button add;
+    AlertDialog dialog;
+    LinearLayout layout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btn = findViewById(R.id.submit);
-        name = findViewById(R.id.nameIn);
-        address = findViewById(R.id.adressIn);
-        phone = findViewById(R.id.phoneIn);
 
+        add = findViewById(R.id.add);
+        layout = findViewById(R.id.container);
 
+        buildDialog();
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.show();
+            }
+        });
     }
 
-    public void addRestaurant(View view){
-        String item = name.getText().toString() + address.getText().toString() + phone.getText().toString();
-        TextView success = findViewById(R.id.success);
-        resList.add(item);
-        Integer man = resList.size();
-        success.setText(man.toString());
-    };
+    private void buildDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        View view = getLayoutInflater().inflate(R.layout.add_restaurant, null);
+
+        final EditText name = view.findViewById(R.id.nameIn);
+
+        builder.setView(view);
+        builder.setTitle("Enter name")
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        addCard(name.getText().toString());
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+
+        dialog = builder.create();
+    }
+
+    private void addCard(String name) {
+        final View view = getLayoutInflater().inflate(R.layout.card, null);
+
+        TextView nameView = view.findViewById(R.id.name);
+        Button delete = view.findViewById(R.id.delete);
+
+        nameView.setText(name);
+
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                layout.removeView(view);
+            }
+        });
+
+        layout.addView(view);
+    }
 }
