9973eeb5029c5c073e22d543f8af40ad044b87f5
Mitesh <mmitesh888@gmail.com>
2022-12-11 23:28:44 -0500

    addeda button that takes you to google maps with the address inputted

--- app/src/main/java/ca/gbc/myapplication/AddRestaurant.java
+++ app/src/main/java/ca/gbc/myapplication/AddRestaurant.java
@@ -2,6 +2,8 @@ package ca.gbc.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -22,10 +24,21 @@ public class AddRestaurant extends AppCompatActivity {
         TextView displayingAddress = findViewById(R.id.textView2);
         TextView displayingPhone = findViewById(R.id.textView3);
         TextView displayingRating = findViewById(R.id.textView4);
+        btn  = findViewById(R.id.goTo);
+
         displayingName.setText(MainActivity.getName());
         displayingAddress.setText(MainActivity.getAddress());
         displayingPhone.setText(MainActivity.getPhone());
         displayingRating.setText(MainActivity.getRating());
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("geo:0,0?q=" +Uri.encode(MainActivity.getAddress())));
+                startActivity(intent);
+            }
+        });
 
     }
 
