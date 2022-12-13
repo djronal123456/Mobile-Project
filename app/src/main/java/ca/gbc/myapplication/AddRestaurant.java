d3be567a97bbdf7d8057fd51b567b462daa6b622
ronal rodriguez arias <newronal@hotmail.com>
2022-12-12 22:36:41 -0500

    Added editing functionality

--- app/src/main/java/ca/gbc/myapplication/AddRestaurant.java
+++ app/src/main/java/ca/gbc/myapplication/AddRestaurant.java
@@ -7,26 +7,36 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class AddRestaurant extends AppCompatActivity {
-    private Button btn;
-    private TextView name,address,phone;
-    ArrayList<String> resList = new ArrayList<>();
-
+    public static String name;
+    public static String address;
+    public static String phone;
+    public static String rating;
+    public static String description;
+    public static Boolean vegetarian;
+    public static Boolean vegan;
+    public static Boolean organic;
+    public static Boolean european;
+    public static Boolean asian;
+    public static Boolean other;
+    public static Boolean trying = Boolean.FALSE;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setContentView(R.layout.display);
         super.onCreate(savedInstanceState);
-        TextView displayingName = findViewById(R.id.textView);
-        TextView displayingAddress = findViewById(R.id.textView2);
-        TextView displayingPhone = findViewById(R.id.textView3);
-        TextView displayingRating = findViewById(R.id.textView4);
-        TextView displayingDescription = findViewById(R.id.textView10);
+        EditText displayingName = findViewById(R.id.textView);
+        EditText displayingAddress = findViewById(R.id.textView2);
+        EditText displayingPhone = findViewById(R.id.textView3);
+        EditText displayingRating = findViewById(R.id.textView4);
+        EditText displayingDescription = findViewById(R.id.textView10);
         TextView displayingTags = findViewById(R.id.textView12);
-        btn  = findViewById(R.id.goTo);
+        Button locationBtn  = findViewById(R.id.goTo);
+        Button saveBtn  = findViewById(R.id.save);
 
         displayingName.setText(MainActivity.getName());
         displayingAddress.setText(MainActivity.getAddress());
@@ -35,15 +45,52 @@ public class AddRestaurant extends AppCompatActivity {
         displayingDescription.setText(MainActivity.getDescription());
         displayingTags.setText(getTags());
 
-        btn.setOnClickListener(new View.OnClickListener() {
+        setName(MainActivity.getName());
+        setAddress(MainActivity.getAddress());
+        setPhone(MainActivity.getPhone());
+        setRating(MainActivity.getRating());
+        setDescription(MainActivity.getDescription());
+        setVegetarian(MainActivity.getVegetarian());
+        setVegan(MainActivity.getVegan());
+        setOrganic(MainActivity.getOrganic());
+        setEuropean(MainActivity.getEuropean());
+        setAsian(MainActivity.getAsian());
+        setOther(MainActivity.getOther());
+
+        locationBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(Intent.ACTION_VIEW);
-                intent.setData(Uri.parse("geo:0,0?q=" +Uri.encode(MainActivity.getAddress())));
-                startActivity(intent);
+               Intent intent = new Intent(Intent.ACTION_VIEW);
+               intent.setData(Uri.parse("geo:0,0?q=" +Uri.encode(MainActivity.getAddress())));
+              startActivity(intent);
             }
         });
 
+        saveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            setTry(Boolean.TRUE);
+                setName(displayingName.getText().toString());
+                setAddress(displayingAddress.getText().toString());
+                setPhone(displayingPhone.getText().toString());
+                setRating(displayingRating.getText().toString());
+                setDescription(displayingDescription.getText().toString());
+                setVegetarian(MainActivity.getVegetarian());
+                setVegan(MainActivity.getVegan());
+                setOrganic(MainActivity.getOrganic());
+                setEuropean(MainActivity.getEuropean());
+                setAsian(MainActivity.getAsian());
+                setOther(MainActivity.getOther());
+                change();
+            }
+
+        });
+
+
+    }
+    public void change(){
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
     }
 
     public static String getTags() {
@@ -68,6 +115,40 @@ public class AddRestaurant extends AppCompatActivity {
         }
         return tags;
     }
+    public static Boolean getTry(){return trying;}
+    public static void setTry(Boolean someTry){trying = someTry;}
+    public static String getName(){return name;}
+    public void setName(String someName){this.name = someName;}
 
+    public static String getAddress(){return address;}
+    public void setAddress(String someAddress){this.address = someAddress;}
 
+    public static String getPhone(){return phone;}
+    public void setPhone(String somePhone){this.phone = somePhone;}
+
+    public static String getRating(){return rating;}
+    public void setRating(String someRating){this.rating=someRating;}
+
+    public static String getDescription(){return description;}
+    public void setDescription(String someDescription){this.description=someDescription;}
+
+    public static Boolean getVegetarian(){return vegetarian;}
+    public void setVegetarian(Boolean someVegetarian){this.vegetarian=someVegetarian;}
+
+    public static Boolean getVegan(){return vegan;}
+    public void setVegan(Boolean someVegan){this.vegan=someVegan;}
+
+    public static Boolean getOrganic(){return organic;}
+    public void setOrganic(Boolean someOrganic){this.organic=someOrganic;}
+
+    public static Boolean getEuropean(){return european;}
+    public void setEuropean(Boolean someEuropean){this.european=someEuropean;}
+
+    public static Boolean getAsian(){return asian;}
+    public void setAsian(Boolean someAsian){this.asian=someAsian;}
+
+    public static Boolean getOther(){return other;}
+    public void setOther(Boolean someOther){this.other=someOther;}
 }
+
+
--- app/src/main/java/ca/gbc/myapplication/MainActivity.java
+++ app/src/main/java/ca/gbc/myapplication/MainActivity.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
-import android.app.Application;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
@@ -30,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
     public static Boolean european;
     public static Boolean asian;
     public static Boolean other;
+    public static Boolean trying;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +45,13 @@ public class MainActivity extends AppCompatActivity {
                 dialog.show();
             }
         });
+        TextView man = findViewById(R.id.tryman);
+        if(AddRestaurant.getTry()){
+            addCard(AddRestaurant.getName(),AddRestaurant.getAddress(),AddRestaurant.getPhone(), AddRestaurant.getDescription(), AddRestaurant.getRating(), AddRestaurant.getVegetarian(), AddRestaurant.getVegan(), AddRestaurant.getOrganic(), AddRestaurant.getEuropean(), AddRestaurant.getAsian(), AddRestaurant.getOther());
+            AddRestaurant.setTry(Boolean.FALSE);
+        }
+        man.setText(AddRestaurant.getTry().toString());
+
     }
 
 
@@ -55,7 +62,8 @@ public class MainActivity extends AppCompatActivity {
         final EditText name = view.findViewById(R.id.nameIn);
         final EditText address = view.findViewById(R.id.adressIn);
         final EditText phone = view.findViewById(R.id.phoneIn);
-        //final EditText rating = view.findViewById(R.id.rating);
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"})
+        final EditText rating = view.findViewById(R.id.ratingIn);
         final EditText description = view.findViewById(R.id.descriptionIn);
         final CheckBox vegetarian = view.findViewById(R.id.tagVegetarian);
         final CheckBox vegan = view.findViewById(R.id.tagVegan);
@@ -65,12 +73,11 @@ public class MainActivity extends AppCompatActivity {
         final CheckBox other = view.findViewById(R.id.tagOther);
 
         builder.setView(view);
-        builder.setTitle("Enter name")
+        builder.setTitle("Enter Details")
                 .setPositiveButton("OK", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        addCard(name.getText().toString(),address.getText().toString(),phone.getText().toString(), description.getText().toString(), vegetarian.isChecked(), vegan.isChecked(), organic.isChecked(), european.isChecked(), asian.isChecked(), other.isChecked());
-
+                        addCard(name.getText().toString(),address.getText().toString(),phone.getText().toString(), description.getText().toString(), rating.getText().toString(), vegetarian.isChecked(), vegan.isChecked(), organic.isChecked(), european.isChecked(), asian.isChecked(), other.isChecked());
                     }
                 })
                 .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
@@ -83,7 +90,7 @@ public class MainActivity extends AppCompatActivity {
         dialog = builder.create();
     }
 
-    private void addCard(String name, String address,String phone, String description, boolean vegan, boolean vegetarian, boolean organic, boolean european, boolean asian, boolean other) {
+    private void addCard(String name, String address,String phone, String description, String rating, boolean vegan, boolean vegetarian, boolean organic, boolean european, boolean asian, boolean other) {
         final View view = getLayoutInflater().inflate(R.layout.card, null);
 
         TextView nameView = view.findViewById(R.id.name);
